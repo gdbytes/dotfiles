776054169847f0b388aca7de5e71ac785cb7606a
Goodbytes <michael@goodbytes.co.uk>
2018-03-30 22:39:37 +0100

    Updated to configure Fish and removed ZSH

--- install.sh
+++ install.sh
@@ -14,30 +14,26 @@ brew update
 brew tap homebrew/bundle
 brew bundle
 
-# Install Oh-My-Zsh
-git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-
-# Make ZSH the default shell environment
-chsh -s $(which zsh)
+# Make Fish the default shell environment
+fish
+echo (which fish) | sudo tee -a /etc/shells
+chsh -s (which fish)
+curl -L http://get.oh-my.fish | fish
+omf install bobthefish
+set -U theme_nerd_fonts yes
 
 # Install Composer
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
 # Install global Composer packages
-/usr/local/bin/composer global require laravel/installer  laravel/valet
-
-# Install Laravel Valet (Fails on High Sierra when trying to install 
-# $HOME/.composer/vendor/bin/valet install
+/usr/local/bin/composer global require laravel/installer
 
 # Create a Sites directories
 mkdir $HOME/Sites
 
 # Create Code directory for Laravel Homestead
 mkdir $HOME/Code
-
-# Copy .zshrc - Will consider symlink later
-cp .zshrc ~/.zshrc
 
 # Copy tmux.conf
 cp tmux.conf ~/.tmux.conf
